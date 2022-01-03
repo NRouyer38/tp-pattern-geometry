d9385661be502cd557183004a29ef950a0bac70e
@nrouyer38 <n.rouyer38@gmail.com>
2022-01-03 14:44:21 +0100

    Add class

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -12,4 +12,15 @@ public class Coordinate {
     public double getY () {
         return y;
     }
+
+    //method coordinate
+    public Coordinate() {
+    	this.x = 0.0;
+    	this.y = 0.0;
+    	}
+    
+    public Coordinate(double x, double y) {
+    	this.x = x;
+    	this.y = y;
+    	}
 }
---
+++ src/main/java/org/acme/geometry/Geometry.java
@@ -0,0 +1,6 @@
+package org.acme.geometry;
+
+public class Geometry {
+
+
+}
