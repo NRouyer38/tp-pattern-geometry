a65802f10b26e7165b52b2d436f700992a4cd9da
@nrouyer38 <n.rouyer38@gmail.com>
2022-01-03 13:59:03 +0100

    add coordinate set get

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -2,4 +2,14 @@ package org.acme.geometry;
 
 public class Coordinate {
 
+    private double x = 0.0;
+    private double y = 0.0;
+
+    public double getX () {
+        return x;
+    }
+
+    public double getY () {
+        return y;
+    }
 }
